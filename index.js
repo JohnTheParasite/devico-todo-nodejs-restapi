b1494fe0543edc9c4e2a7f0d5db851417e817dcd
i.barbashov <ivan.barbashov@gmail.com>
2022-06-07 13:01:34 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const tasks = [
+  { id: 1, done: false, content: "First task" },
+];
+
+function validateCourse(body) {
+  let result = { error: false, message: ''};
+
+  if (!body.content) {
+    result = { error: true, message: 'Content is required'};
+  }
+
+  return result
+}
+
+app.get('/api/tasks', (req, res) => {
+  res.send(tasks);
+})
+
+app.get('/api/tasks/:id', (req, res) => {
+  const task = tasks.find((el) => el.id === parseInt(req.params.id))
+  if (!task) return res.status(404).send('The task with the given ID was not found.');
+
+  res.send(tasks);
+})
+
+app.post('/api/tasks', (req, res) => {
+  const validation = validateCourse(req.body);
+  if (validation.error) return res.status(400).send(validation.message);
+
+  const task = {
+    id: Math.max(...tasks.map((el) => el.id)) + 1,
+    done: false,
+    content: req.body.content
+  };
+  tasks.push(task);
+  res.send(task)
+})
+
+app.put('/api/tasks/:id', (req, res) => {
+  const task = tasks.find((el) => el.id === parseInt(req.params.id))
+  if (!task) return res.status(404).send('The task with the given ID was not found.');
+
+  const { error, message } = validateCourse(req.body);
+  if (error) return res.status(400).send(message);
+
+  const { done, content } = req.body
+  task.done = done ? done : task.done;
+  task.content = content;
+  res.send(task);
+})
+
+app.delete('/api/tasks/:id', (req, res) => {
+
+  const task = tasks.find((el) => el.id === parseInt(req.params.id))
+  if (!task) return res.status(404).send('The task with the given ID was not found.');
+
+  const index = tasks.indexOf(task)
+  tasks.splice(index, 1);
+
+  res.send(task);
+
+})
+
+const port = process.env.PORT || 8081;
+app.listen(port, () => console.log(`Listening on port ${port}...`))
